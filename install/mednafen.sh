33dd8a0c23bf9b5691b7db306ed9800dac229d06
Alexandre Girard <git@alexgirard.com>
2016-11-28 22:35:01 +0100

    correct mednafen install script

--- install/mednafen.sh
+++ install/mednafen.sh
@@ -1,4 +1,4 @@
+DIR=$dirname $(readlink -f $0))
 sudo apt-get update
 sudo apt-get -y install mednafen libsdl2-dev
-tar -xzf ../config/mednafen.tgz -C $HOME
-mkdir $HOME/roms
+cp -ar $DIR/../config/mednafen $HOME/.mednafen
