ffc854e226c38ec43f7e26f203f492a06e797a14
Alexandre Girard <git@alexgirard.com>
2016-11-28 23:42:44 +0100

    add retroarch icon

--- install/retroarch.sh
+++ install/retroarch.sh
@@ -7,3 +7,5 @@ git clone git://github.com/libretro/RetroArch.git
 cd RetroArch/
 ./configure --enable-opengles --disable-oss --disable-sdl --disable-ffmpeg --disable-vg --disable-cg --enable-neon --enable-floathard
 sudo make install
+jq '.pages[0].items |= . + [{shell: "retroarch", name: "RetroArch", icon: "/home/chip/chip_family/appIcons/retroarch.png"}]' $HOME/.pocket-home/config.json > $HOME/.pocket-home/config.json
+
